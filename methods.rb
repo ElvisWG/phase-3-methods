9d4c3249d4ec18fc2b0656ec2fa76f3979553915
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-10-18 22:20:22 +0300

    Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,39 @@
 # Your code here!
+# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+# second method
+def greet (person)
+    puts ("Hello, #{person}!")
+end
+
+
+# third method
+
+def greet_with_default person = 'programmer'
+    puts "Hello, #{person}!"
+end
+
+greet_with_default
+
+
+# fourth method 
+
+def add(num1, num2)
+  return (num1 + num2)
+end
+
+puts add(1,2)
+
+
+def halve(value)
+    if (value.class != Integer)
+        return nil
+    else 
+        return value/2
+    end
+end
